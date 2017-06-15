94f87d94207983ea6ba3dbe8075d3beae19f4d19
bromne <bromne@gmail.com>
2017-06-15 23:47:04 +0900

    extracts retweeted user name.

--- src/app/tweet.ts
+++ src/app/tweet.ts
@@ -1,6 +1,8 @@
 import { DateTime } from 'date-time-js';
 
 export class Tweet {
+    static RETWEET: RegExp = /^RT @([a-zA-Z0-9_]+)\:/;
+
     public user_name : string;
     public retweeted_user_name : string | null;
 
@@ -16,6 +18,15 @@ export class Tweet {
         public retweeted_status_timestamp: DateTime | null,
         public expanded_urls: string | null,
     ) {
-        // code...
+        if (this.isRetweet()) {
+            let result = this.text.match(Tweet.RETWEET);
+            this.retweeted_user_name = result ? result[1] : "";
+        } else {
+            this.retweeted_user_name = null;
+        }
+    }
+
+    public isRetweet(): boolean {
+        return this.retweeted_status_id !== null;
     }
 }
