38283ee724a6261e387f9fe8af90cd90e3831376
bromne <bromne@gmail.com>
2017-06-21 13:38:25 +0900

    coordinate styles for application component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit, ElementRef } from '@angular/core';
 import { TweetService, RequestProfile, TweetResult } from 'app/tweet.service'
 import { DateTime } from "date-time-js/date-time";
 import { TweetComponent } from "app/tweet.component";
@@ -8,30 +8,38 @@ import { LocalDate } from "js-joda/dist/js-joda";
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css'],
+    styleUrls: ['./app.component.scss'],
     providers: [TweetService],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit {
 
     @ViewChild(CalendarComponent)
     calendar: CalendarComponent;
 
     tweetResult: TweetResult | null = null;
 
-    constructor(private tweetService: TweetService) {
+    constructor(private tweetService: TweetService, private elementRoot: ElementRef) {
     }
 
-    ngOnInit() {
+    ngOnInit(): void {
         let date = LocalDate.of(2012, 3, 1);
         this.calendar.value = date;
+        this.onDateChange(date);
+    }
 
+    ngAfterViewInit(): void {
+        this.elementRoot.nativeElement
+    }
+
+    onDateChange(date: LocalDate) {
+        this.tweetResult = null;
         let request = new RequestProfile("takeda25", date);
         this.tweetService.findTweetsByDate(request)
             .then(result => {
                 this.tweetResult = result;
             })
             .catch(e => {
-                console.log(e);
+                this.tweetResult = new TweetResult(request, []);
             });
     }
 }
