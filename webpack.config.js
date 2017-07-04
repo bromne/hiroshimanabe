655f5a95af3b1e3328880d826db2ffa1be342beb
bromne <bromne@gmail.com>
2017-07-04 17:29:27 +0900

    React works.

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,29 @@
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+// CopyWebpackPlugin のファイル数制限を回避します
+var fs = require('fs');
+var gracefulFs = require('graceful-fs');
+gracefulFs.gracefulify(fs);
 
 module.exports = {
+    context: path.resolve(__dirname, 'src'),
     entry: {
-        bundle: './src/app.tsx'
+        bundle: './app.tsx'
     },
     output: {
-        path: path.join(__dirname, 'dist'),
-        filename: 'bundle.js'
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
     },
+    plugins: [
+        new CopyWebpackPlugin([
+            'assets',
+            'data',
+            'index.html'
+        ].map(function (path) {
+            return { from: path, to: path };
+        }))
+    ],
     devtool: "source-map",
     resolve: {
         extensions: [".ts", ".tsx", ".js", ".json"]
@@ -28,7 +44,7 @@ module.exports = {
             }
         ],
         loaders: [
-            { test : /\.(?:html|ico)/}
+            { test : /\.(?:html|ico|json|png)/, loader: "file" }
         ]
     }
 };
