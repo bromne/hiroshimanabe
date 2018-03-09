bb47a60b6985a6c8754a153b73db05338a425ba1
bromne <bromne@gmail.com>
2018-03-09 13:56:01 +0900

    build script now copies data directory.

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path')
+
+module.exports = {
+    configureWebpack: {
+        plugins: [
+            new CopyWebpackPlugin([
+                {
+                  from: path.resolve(__dirname, './src/data'),
+                  to: 'data',
+                  ignore: ['.*'],
+                }
+            ])
+        ]
+    }
+};
